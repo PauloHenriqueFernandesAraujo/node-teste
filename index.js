91cba8fdaa5aa0b770418b5e71eea1c3d42d3ac9
PauloHenriqueFernandesAraujo <paulohenriquefernandesdearaujo@gmail.com>
2021-10-10 22:46:10 -0300

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // run `node index.js` in the terminal
+const koa = require('koa2');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+app = new Koa();
+
+app.use((ctx) => {
+  ctx.body = 'Hello Koa';
+});
+
+app.listen(3000).then((result) => console.log('serve is runnig on:' + 3000));
